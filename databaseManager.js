4de4863ba9e9264fa47820aa710b430ade804fde
Mattiamoma <mattiama03@gmail.com>
2024-09-10 21:18:42 +0200

    feat: Add shortener functionality and redirect to original full URL
    - Added functionality to shorten URLs and redirect to the original full URL.
    - Updated the `databaseManager.js` file to include methods for getting the short URL, getting the full URL, adding a new URL, and incrementing clicks.
    - Updated the `server.js` file to handle GET and POST requests for shortening and redirecting URLs.
    - Added the `body-parser` dependency to the `package.json` and `package-lock.json` files.
    - Removed the `index.ejs` file from the `view` directory and added a new `index.ejs` file in the `views` directory.
    - Ignored the `db/` directory in the `.gitignore` file.

--- databaseManager.js
+++ databaseManager.js
@@ -1,4 +1,5 @@
 const sqlite = require('sqlite3').verbose();
+const nanoid = require('nanoid');
 
 
 class DatabaseManager {
@@ -10,6 +11,63 @@ class DatabaseManager {
             console.log('Connected to the urls database.');
         });
         this.db.run('CREATE TABLE IF NOT EXISTS urls(fullUrl TEXT PRIMARY KEY, shortUrl TEXT, clicks INTEGER DEFAULT 0)');
+    }
+
+
+
+    getShortUrl(fullUrl) {
+        this.db.get('SELECT shortUrl FROM urls WHERE fullUrl = ?', [fullUrl], (err, row) => {
+            if (err || !row) {
+                return undefined;
+            } else {
+                return row.shortUrl;
+            }
+        });
+    }
+
+
+    getFullUrl(shortUrl, callback) {
+        this.db.get('SELECT fullUrl FROM urls WHERE shortUrl = ?', [shortUrl], (err, row) => {
+            if (err || !row) {
+                callback(undefined);
+            } else {
+                callback(row.fullUrl);
+            }
+        });
+    }
+
+
+
+    addUrl(fullUrl, callback) {
+        let searchShort = this.getShortUrl(fullUrl);
+        if (searchShort) {
+            callback(searchShort);
+        } else {
+            let shortUrl = nanoid.nanoid(12);
+
+            this.db.run('INSERT INTO urls(fullUrl, shortUrl) VALUES(?, ?)', [fullUrl, shortUrl], (err) => {
+                if (err) {
+                    console.error(err.message);
+                    callback(err);
+                } else {
+                    console.log('Added new URL to database');
+                    callback(shortUrl);
+                }
+            });
+        }
+    }
+    
+
+    incrementClicks(shortUrl, callback) {
+        this.db.run('UPDATE urls SET clicks = clicks + 1 WHERE shortUrl = ?', [shortUrl], (err) => {
+            if (err) {
+                console.error(err.message);
+                callback(err);
+            } else {
+                console.log('Incremented clicks for short URL:', shortUrl);
+                callback(true);
+            }
+        });
     }
 
 
--- server.js
+++ server.js
@@ -1,15 +1,58 @@
 const express = require('express');
 const app = express();
 const databaseManager = require('./databaseManager');
-db = new databaseManager();
+const db = new databaseManager();
 
-
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.set('view engine', 'ejs');
-app.get('/', (req, res) => {
+
+
+app.get('/', async (req, res) => {
+    console.log('GET /');
     res.render('index');
 });
+
+
+app.post('/shorten', async (req, res) => {
+
+    let fullUrl = req.body.fullUrl;
+    if(!fullUrl){
+        res.send({
+            status: 400,
+            message: 'URL is required'
+        });
+        return;
+    }
+    db.addUrl(fullUrl, (shortUrl) => {
+        if (shortUrl) {
+            shortUrl = `${req.protocol}://${req.get('host')}/${shortUrl}`;
+            res.send({
+                status: 200,
+                shortUrl: shortUrl
+            });
+        } else {
+            res.send({
+                status: 500,
+                message: 'Failed to shorten URL, please try again'
+            });
+        }
+    });
+});
+
+app.get('/:shortUrl', async (req, res) => {
+    const shortUrl = req.params.shortUrl;
+    
+    db.getFullUrl(shortUrl, (fullUrl) => {
+        if (fullUrl) {
+            res.redirect(fullUrl);
+        }
+        else {
+            res.status(404).send('URL not found');
+        }
+    });
+});
+
 
 
 app.listen(process.env.PORT || 3000, () => {
